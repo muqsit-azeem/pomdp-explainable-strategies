e5de3b2c0a182012c457c433f63e17e0989d36b6
muqsit <muqsit.azeem@tum.de>
2024-08-20 10:25:49 +0200

    explainable-mealy-machine

---
+++ quantitative/paper-case-study-heart/explainable-mealy-machine/heart-disease/memory-transitions/0/default.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+float classify(const float x[]);
+
+int main() {
+    float x[] = {1.f,0.f,1.f,2.f,0.f,1.f,0.f};
+    float result = classify(x);
+    return 0;
+}
+
+float classify(const float x[]) {
+	if (x[6] <= 0.5) {
+		if (x[3] <= 0.5) {
+			if (x[2] <= 1.5) {
+				return 0.0f;
+			}
+			else {
+				return 9.0f;
+			}
+
+		}
+		else {
+			return 9.0f;
+		}
+
+	}
+	else {
+		return 3.0f;
+	}
+
+}
