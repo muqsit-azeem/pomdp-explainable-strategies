b8d41e81b2634a8cb0d7d06d9c823899e061a059
Muqsit Azeem <muqsit.azeem@tum.de>
2025-05-03 13:55:01 -0700

    Remove storm submodule

---
+++ qualitative/paper-case-studies-iterative/run_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# virtual environment path
+VENV_PATH="../../../venv/bin/activate"
+
+# activate the virtual environment where dtcontrol is installed
+echo "Activating virtual environment..."
+source $VENV_PATH
+
+# run storm iterative solver
+echo "Running run_all.py..."
+python3 run_all.py
+
+# check for successful run
+if [ $? -ne 0 ]; then
+    echo "run_all.py failed. Exiting."
+    exit 1
+fi
+
+echo "All scripts ran successfully."
